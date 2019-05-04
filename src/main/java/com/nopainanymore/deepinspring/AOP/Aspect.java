976fdbddefe4b45060ca2c183d0ba690063f33c9
Lee875083146 <vbof53@163.com>
2019-05-04 15:42:21 +0800

    Simple AOP Impl

--- src/main/java/com/nopainanymore/deepinspring/AOP/Aspect.java
+++ src/main/java/com/nopainanymore/deepinspring/AOP/Aspect.java
@@ -1,10 +1,14 @@
 package com.nopainanymore.deepinspring.AOP;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * deep-in-spring: Aspect
@@ -17,12 +21,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class Aspect {
 
+    private static final Gson gson = new Gson();
+
     @Pointcut("execution(public * com.nopainanymore.deepinspring.AOP.AOPController.*(..)))")
     public void pointCut() {
     }
 
     @Before("pointCut()")
-    public void doBefore() {
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        log.info("Aspect- doBefore:{}", gson.toJson(requestAttributes.getRequest().getRequestedSessionId()));
+        log.info("Aspect- doBefore:{}", joinPoint.toShortString());
         log.info("Aspect- doBefore- before the pointCut");
     }
 
