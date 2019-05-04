e87fa5f06e4b84b437cd099ddf81a5dabd24f588
Lee875083146 <vbof53@163.com>
2019-05-04 15:24:24 +0800

    Simple AOP Impl

--- src/main/java/com/nopainanymore/deepinspring/AOP/Aspect.java
+++ src/main/java/com/nopainanymore/deepinspring/AOP/Aspect.java
@@ -1,10 +1,36 @@
 package com.nopainanymore.deepinspring.AOP;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * deep-in-spring: Aspect
  *
  * @author NoPainAnymore
  * @date 2019-05-04 14:48
  */
+@org.aspectj.lang.annotation.Aspect
+@Component
+@Slf4j
 public class Aspect {
+
+    @Pointcut("execution(public * com.nopainanymore.deepinspring.AOP.AOPController.*(..)))")
+    public void pointCut() {
+    }
+
+    @Before("pointCut()")
+    public void doBefore() {
+        log.info("Aspect- doBefore- before the pointCut");
+    }
+
+    @After("pointCut()")
+    public void doAfter() {
+        log.info("Aspect- doAfter- after the pointCut");
+
+    }
+
+
 }
