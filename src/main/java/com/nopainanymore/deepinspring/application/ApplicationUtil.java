ca49eed33b678f5efdbeb0b27e57e58f9f040594
Lee875083146 <vbof53@163.com>
2019-05-11 22:21:16 +0800

    add ApplicationUil name

---
+++ src/main/java/com/nopainanymore/deepinspring/application/ApplicationUtil.java
@@ -0,0 +1,83 @@
+package com.nopainanymore.deepinspring.application;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * deep-in-spring: ApplicationUtil
+ *
+ * @author NoPainAnymore
+ * @date 2019-05-11 22:19
+ */
+@Component
+@Slf4j
+public class ApplicationUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        log.info("------ApplicationUtil setApplicationContext-------");
+        ApplicationUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 根据类名获取到bean
+     *
+     * @param <T>
+     * @param clazz
+     * @return
+     * @throws BeansException
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBeanByClass(Class<T> clazz) throws BeansException {
+        try {
+            return (T) applicationContext.getBean(upperToLowerCamelCase(clazz.getSimpleName()));
+        } catch (NoSuchBeanDefinitionException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("ApplicationUtil- getBeanByClass", e);
+            }
+            log.info("ApplicationUtil- getBeanByClass- NoSuchBeanDefinitionException:{}", e.getMessage());
+            return null;
+        }
+    }
+
+    private static String upperToLowerCamelCase(String upperCamelCase) {
+        char[] cs = upperCamelCase.toCharArray();
+        cs[0] += 32;//首字母大写到小写
+        return String.valueOf(cs);
+    }
+
+    public static Object getBeanBySimpleName(String clazzSimpleName) {
+        try {
+            return applicationContext.getBean(upperToLowerCamelCase(clazzSimpleName));
+        } catch (NoSuchBeanDefinitionException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("ApplicationUtil- getBeanByClass", e);
+            }
+            log.info("ApplicationUtil- getBeanByClass- clazzSimpleName:{} NoSuchBeanDefinitionException:{}", clazzSimpleName, e.getMessage());
+            return null;
+        }
+    }
+
+    public static boolean containsBean(Class clazz) {
+        return applicationContext.containsBean(upperToLowerCamelCase(clazz.getSimpleName()));
+    }
+
+    public static boolean containsBean(String simpleName) {
+        return applicationContext.containsBean(upperToLowerCamelCase(simpleName));
+    }
+
+    public static boolean isSingleton(String simpleName) throws NoSuchBeanDefinitionException {
+        return applicationContext.isSingleton(upperToLowerCamelCase(simpleName));
+    }
+}
